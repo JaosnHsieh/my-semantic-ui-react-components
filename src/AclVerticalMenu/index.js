c7682537985473b2c0ca1033386b3245d68ce49e
jasonhsieh <appleoxxo@gmail.com>
2018-12-16 12:04:37 +0800

    add AclVerticalMenu usage

--- src/AclVerticalMenu/index.js
+++ src/AclVerticalMenu/index.js
@@ -1,61 +1,63 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
-import { Icon, Menu } from "semantic-ui-react";
-import styles from "./VerticalMenu.css";
-import { headerHeight, leftSidebarWidth } from "./SidebarLayout";
+import { Icon, Menu, Dropdown, Button } from "semantic-ui-react";
+// import styles from './VerticalMenu.css';
+// import { headerHeight } from './DmpSidebarLayout';
 
 // permission string is according to the AclRole data in mongodb
 const menuItems = [
   {
-    name: "project",
     icon: "object group outline",
     displayName: "Project",
-    subMenuItems: [
-      {
-        name: "myprojects",
-        displayName: "My projects",
-        routePath: "/projects",
-        otherActiveRegexArr: [/\/project\/edit/i] // don't put g flag because of regex lastIndex
-      }
-    ],
-    acl: "project"
+    acl: "project",
+    routePath: "/projects",
+    otherActiveRegexArr: [/\/project\/edit/i, /\/project\/\w+$/i] // don't put g flag because of regex lastIndex
+    // subMenuItems: [
+    //   {
+    //     name: 'myprojects',
+    //     displayName: 'My projects',
+    //     routePath: '/projects',
+
+    //   }
+    // ],
   },
   {
-    name: "report",
-    icon: "chart line",
+    icon: "chart area",
     displayName: "Report",
-    subMenuItems: [
-      {
-        name: "progress",
-        displayName: "Overview",
-        routePath: "/projects/progress"
-      },
-      {
-        name: "template",
-        displayName: "Template",
-        routePath: "/templates",
-        otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
-      }
-    ],
+    routePath: "/projects/progress",
     acl: "project"
+    // subMenuItems: [
+
+    //   {
+    //     name: 'template',
+    //     displayName: 'Template',
+    //     routePath: '/templates',
+    //     otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+    //   }
+    // ],
   },
   {
-    name: "audience",
+    icon: "chart line",
+    displayName: "Template",
+    routePath: "/templates",
+    acl: "project",
+    otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+  },
+  {
     icon: "users",
     displayName: "Audience",
     routePath: "/people",
     acl: "audience"
   },
   {
-    name: "location",
-    icon: "location arrow",
+    icon: "marker",
     displayName: "Location",
     routePath: "/location",
     otherActiveRegexArr: [/\/location/i],
     acl: "location"
   },
   {
-    name: "userManagement",
     icon: "user",
     displayName: "User Management",
     routePath: "/user-management",
@@ -63,13 +65,61 @@ const menuItems = [
     acl: "permission"
   }
 ];
+
+//from  https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+
+function storageAvailable(type) {
+  try {
+    var storage = window[type],
+      x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0
+    );
+  }
+}
+
 class VerticalMenu extends Component {
   constructor() {
     super();
     this.state = {
-      activeItemName: "myprojects"
+      activeItemName: "myprojects",
+      isCollapsed: this.getIsCollapsedFromLocalStorage(),
+      isDropdownMenusOn: {}
     };
   }
+  // localStroage accept string value only
+  getIsCollapsedFromLocalStorage = () => {
+    const defaultIsCollapsed = true;
+    if (storageAvailable("localStorage")) {
+      return localStorage.getItem("isCollapsed") === "false"
+        ? false
+        : defaultIsCollapsed;
+    } else {
+      return defaultIsCollapsed;
+    }
+  };
+  // localStroage accept string value only
+  saveIsCollapsedToLocalStorage = (bool = true) => {
+    if (storageAvailable("localStorage")) {
+      localStorage.setItem("isCollapsed", bool.toString());
+    }
+  };
+
   handleItemClick = item => {
     this.switchRoute(item.routePath);
   };
@@ -102,15 +152,171 @@ class VerticalMenu extends Component {
 
     return false;
   };
+  setMenusOpen = (index = 0, bool = false) => {
+    this.setState(currentState => {
+      return {
+        isDropdownMenusOn: {
+          ...currentState.isDropdownMenusOn,
+          [index]: bool
+        }
+      };
+    });
+  };
 
   render() {
-    // const { activeItemName } = this.state;
-    const { activeItemName } = this.props;
+    const { isCollapsed, isDropdownMenusOn } = this.state;
+    const leftSidebarWidth = isCollapsed ? "5.5rem" : "20rem";
+    const { activeItemName, menuItems, headerHeight } = this.props;
     const pathname = window.location.pathname;
+    const renderMenuItems = [];
+
+    if (isCollapsed) {
+      menuItems.forEach((item, index) =>
+        // me.can(item.acl) &&
+        renderMenuItems.push(
+          <div key={index} style={{ position: "relative", height: "60.41px" }}>
+            <Icon
+              size={"large"}
+              name={item.icon}
+              style={{
+                position: "absolute",
+                left: "20.5px",
+                top: "16.5px"
+              }}
+            />
+            <Dropdown
+              style={{ height: "60.41px" }}
+              className={this.isItemActive(pathname, item) ? "active" : ""}
+              item
+              icon={""} //for remove down arrow icon
+              direction="right"
+              open={_.get(isDropdownMenusOn, index, false)}
+              onMouseEnter={() => {
+                this.setMenusOpen(index, true);
+              }}
+              onMouseLeave={() => {
+                this.setMenusOpen(index, false);
+              }}
+              onClick={
+                item.routePath
+                  ? e => {
+                      this.handleItemClick(item);
+                    }
+                  : undefined
+              }
+            >
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  style={{
+                    cursor: "unset",
+                    borderLeft: "2px solid rgb(54, 138, 253)"
+                  }}
+                  // active={this.isItemActive(pathname, item)}
+                  // onClick={
+                  //   item.routePath
+                  //     ? e => {
+                  //         this.handleItemClick(item);
+                  //       }
+                  //     : undefined
+                  // }
+                >
+                  <Dropdown.Header content={`${item.displayName}`} />
+                  <Dropdown.Divider />
+                </Dropdown.Item>
+                {item.subMenuItems &&
+                  item.subMenuItems.map((subItem, subIndex) => (
+                    <Dropdown.Item
+                      key={subIndex}
+                      active={this.isItemActive(pathname, subItem)}
+                      onClick={e => {
+                        this.handleItemClick(subItem);
+                      }}
+                    >
+                      {subItem.displayName}
+                    </Dropdown.Item>
+                  ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        )
+      );
+    } else {
+      //expanded menus
+      menuItems.forEach((item, index) =>
+        // me.can(item.acl) &&
+        renderMenuItems.push(
+          <Menu.Item
+            key={index}
+            onClick={
+              item.routePath
+                ? e => {
+                    this.handleItemClick(item);
+                  }
+                : undefined
+            }
+            active={this.isItemActive(pathname, item)}
+          >
+            <Icon
+              // styleName="my-icon"
+              style={{
+                float: "none",
+                margin: "0 .5em 0 0"
+              }}
+              size={"large"}
+              name={item.icon}
+            />
+            {item.displayName}
+
+            {item.subMenuItems &&
+              item.subMenuItems.map((subItem, subIndex) => (
+                <Menu.Menu
+                  // styleName="my-menu"
+                  style={{ textAlign: "center" }}
+                  key={subIndex}
+                >
+                  <Menu.Item
+                    // name={subItem.name}
+                    active={this.isItemActive(pathname, subItem)}
+                    onClick={e => {
+                      this.handleItemClick(subItem);
+                    }}
+                  >
+                    {subItem.displayName}
+                  </Menu.Item>
+                </Menu.Menu>
+              ))}
+          </Menu.Item>
+        )
+      );
+    }
+
+    const collapseSwitchItem = (
+      <div
+        style={{
+          position: "fixed",
+          width: leftSidebarWidth,
+          bottom: 0,
+          borderTop: "2px solid rgb(227, 230, 234)",
+          textAlign: "right",
+          cursor: "pointer"
+        }}
+        onClick={() => {
+          this.setState({ isCollapsed: !isCollapsed });
+          this.saveIsCollapsedToLocalStorage(!isCollapsed);
+        }}
+      >
+        <Icon
+          name={isCollapsed ? "angle double right" : "angle double left"}
+          size={"big"}
+          color={"grey"}
+        />
+      </div>
+    );
     return (
-      <div className="menu-sidebar">
+      <div style={{ width: leftSidebarWidth }}>
         <Menu
           style={{
+            // paddingTop: isCollapsed ? '52px' : '0',
             marginTop: headerHeight,
             width: "100%",
             maxWidth: leftSidebarWidth
@@ -120,46 +326,98 @@ class VerticalMenu extends Component {
           fixed={"left"}
           vertical
         >
-          {/* <Menu.Item>
-            <Input placeholder="Search..." />
-          </Menu.Item> */}
-          {menuItems.map(
-            (item, index) =>
-              me.can(item.acl) && (
-                <Menu.Item
-                  key={index}
-                  onClick={
-                    item.routePath
-                      ? e => {
-                          this.handleItemClick(item);
-                        }
-                      : undefined
-                  }
-                  active={this.isItemActive(pathname, item)}
-                >
-                  <Icon styleName="my-icon" size={"big"} name={item.icon} />
-                  {item.displayName}
-                  {item.subMenuItems &&
-                    item.subMenuItems.map((subItem, subIndex) => (
-                      <Menu.Menu styleName="my-menu" key={subIndex}>
-                        <Menu.Item
-                          name={subItem.name}
-                          active={this.isItemActive(pathname, subItem)}
-                          onClick={e => {
-                            this.handleItemClick(subItem);
-                          }}
-                        >
-                          {subItem.displayName}
-                        </Menu.Item>
-                      </Menu.Menu>
-                    ))}
-                </Menu.Item>
-              )
-          )}
+          {renderMenuItems}
+          {collapseSwitchItem}
         </Menu>
       </div>
     );
   }
 }
 
+VerticalMenu.propTypes = {
+  isCollapsed: PropTypes.bool,
+  menuItems: PropTypes.array,
+  headerHeight: PropTypes.string
+};
+
+VerticalMenu.defaultProps = {
+  isCollapsed: true,
+  menuItems: [],
+  headerHeight: "200px"
+};
 export default VerticalMenu;
+
+export const Usage = () => (
+  <VerticalMenu
+    isCollapsed={true}
+    headerHeight={"200px"}
+    menuItems={[
+      {
+        icon: "object group outline",
+        displayName: "Group",
+        // acl: "project",
+        routePath: "/group",
+        otherActiveRegexArr: [/\/project\/edit/i, /\/project\/\w+$/i] // don't put g flag because of regex lastIndex
+      },
+      {
+        icon: "chart area",
+        displayName: "Chart",
+        routePath: "/chart"
+        // acl: "project"
+        // subMenuItems: [
+
+        //   {
+        //     name: 'template',
+        //     displayName: 'Template',
+        //     routePath: '/templates',
+        //     otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+        //   }
+        // ],
+      },
+      {
+        icon: "chart line",
+        displayName: "Chart chart",
+        routePath: "/Chartchart",
+        // acl: "project",
+        otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+      }
+    ]}
+  />
+);
+
+export const Usage2 = () => (
+  <VerticalMenu
+    isCollapsed={false}
+    headerHeight={"150px"}
+    menuItems={[
+      {
+        icon: "object group outline",
+        displayName: "Group",
+        // acl: "project",
+        routePath: "/group",
+        otherActiveRegexArr: [/\/project\/edit/i, /\/project\/\w+$/i] // don't put g flag because of regex lastIndex
+      },
+      {
+        icon: "chart area",
+        displayName: "Chart",
+        routePath: "/chart",
+        // acl: "project"
+        subMenuItems: [
+          {
+            name: "submenu1",
+            displayName: "submenu1",
+            routePath: "/submenu1",
+            otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+          }
+        ]
+      },
+      {
+        icon: "chart line",
+        displayName: "Chart chart",
+        routePath: "/Chartchart",
+        // acl: "project",
+        otherActiveRegexArr: [/^\/templates\/add$/i] // don't put g flag because of regex lastIndex
+      }
+    ]}
+  />
+);
